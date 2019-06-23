cd1ffed4da843864d175ace3dbfec1522b276b4b
Etienne Dechamps <etienne@edechamps.fr>
2019-06-23 16:18:23 +0100

    Make it possible to find version 6 of Inno Setup.

--- FindInnoSetup.cmake
+++ FindInnoSetup.cmake
@@ -2,6 +2,8 @@ find_program(InnoSetup_iscc_EXECUTABLE iscc
     HINTS
         # Sadly, Inno Setup doesn't seem to have a clean registry entry for the
         # installation path. This is the best we got.
+		"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Inno Setup 6_is1;InstallLocation]"
+        "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Inno Setup 6_is1;InstallLocation]"
         "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Inno Setup 5_is1;InstallLocation]"
         "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Inno Setup 5_is1;InstallLocation]"
 )
